0106838d8d03ee69683bd71c21380c57d30ca3fd
Cupsa Darius <cupsadarius@gmail.com>
2017-12-09 02:03:00 +0200

    Initial commit

---
+++ lib/around25.ex
@@ -0,0 +1,18 @@
+defmodule Around25 do
+  @moduledoc """
+  Documentation for Around25.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Around25.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/around25_test.exs
@@ -0,0 +1,8 @@
+defmodule Around25Test do
+  use ExUnit.Case
+  doctest Around25
+
+  test "greets the world" do
+    assert Around25.hello() == :world
+  end
+end
